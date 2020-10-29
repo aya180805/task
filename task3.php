beffb3a0865fdb29bd1383b467eb8f6c6d05ff6a
aya180805 <aya.r180805@gmail.com>
2020-10-29 05:45:44 +0000

    PHP4

--- task3.php
+++ task3.php
@@ -5,6 +5,7 @@ function y($x){
     return $result;
 }
 echo y(1);
+echo "\n";
 
 //課題2 $a と $b を仮引数に持ち、　$a と $b　を足した結果を返す関数を作成してください。
 function f($a,$b){
@@ -12,30 +13,38 @@ function f($a,$b){
     return $result2;
 }
 echo f(2,3);
+echo "\n";
 
 //課題3 $arr という配列の仮引数を持ち、数値が入った配列array(1, 3, 5 ,7, 9) を渡すとその要素をすべてかけた結果を返す関数を作成してください。
-$array=array(1,3,5,7,9);
-$result3=$array[0];
-for($i=1;$i<count($array);$i++){
-    $result3*=$array[$i];
+$arr=array(1,3,5,7,9);
+function m($arr){
+$result3=1;
+foreach($arr as $a){
+    $result3*=$a;
 }
-echo $result3;
-
-?>
+    return $result3;
+}
+echo m($arr);
+echo "\n";
 
 //課題4 【応用】　次のプログラムは、配列の中で1番大きい値を返す max_array という関数を実装しようとしています。途中の部分を完成させてください。
 function max_array($arr){
     $max_number=$arr[0];
     foreach($arr as $a){
-        
+        if($max_number < $a){
+            $max_number=$a;
+        }
     }
     return $max_number;
 }
-echo max_array()
+echo max_array(array(67,23,86,54,56,38,57));
+echo "\n";
+
+?>
 
 //課題5 次のビルトイン関数の用途、使い方を調べて実際に使ってみてください。
-#strip_tags : 文字列から HTML および PHP タグを取り除く。
-#array_push : 一つ以上の要素を配列の最後に追加する。
-#array_merge : 指定した複数の配列を1つにマージする。
-#time, mktime : 日付を Unix のタイムスタンプとして取得する。
-#date : ローカルの日付/時刻を書式化する。
+#strip_tags : 指定した文字列から HTML および PHP タグを取り除く。
+#array_push : 一つ以上の要素を指定した配列の末尾に追加する。
+#array_merge : 指定した複数の配列を1つに結合する。
+#time, mktime : 現在の時刻を取得する、指定した日時からUnixタイムスタンプを取得する。
+#date : 日付/時刻/曜日を表示する。
